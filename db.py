1204957635a6e64fb37e1777f9bcb43298d4399f
AnanasyQ <dyadchenko1607@gmail.com>
2026-02-01 17:17:03 +0200

    Create DataBase

--- db.py
+++ db.py
@@ -1,1 +1,33 @@
 # Робота локальної бази даних SQLite
+import sqlite3
+
+def init_db(conn):
+    conn.execute("PRAGMA foreign_keys = ON")
+    conn.executescript(""" CREATE TABLE IF NOT EXISTS cartridges(
+                 id INTEGER PRIMARY KEY,
+                 name TEXT NOT NULL,
+                 archive INTEGER NOT NULL DEFAULT 0,
+                 note TEXT);
+
+                 CREATE TABLE IF NOT EXISTS history(
+                 cartridge_id INTEGER NOT NULL,
+                 date TEXT NOT NULL, -- ISO: YYYY-MM-DD
+                 FOREIGN KEY(cartridge_id) REFERENCES cartridges(id)
+                       ON UPDATE CASCADE ON DELETE CASCADE);
+
+                 CREATE TABLE IF NOT EXISTS availability(
+                 cartridge_id INTEGER NOT NULL,
+                 stock INTEGER NOT NULL,
+                 empty INTEGER NOT NULL,
+                 used INTEGER NOT NULL,
+                 FOREIGN KEY(cartridge_id) REFERENCES cartridges(id)
+                       ON UPDATE CASCADE ON DELETE CASCADE);
+
+                 CREATE TABLE IF NOT EXISTS statistics(
+                 cartridge_id INTEGER NOT NULL,
+                 month INTEGER NOT NULL, -- 1..12
+                 count INTEGER NOT NULL,
+                 FOREIGN KEY(cartridge_id) REFERENCES cartridges(id)
+                       ON UPDATE CASCADE ON DELETE CASCADE);
+                 """)
+    conn.commit()
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Головний файл запуску
+import sqlite3
+import db
+
+conn = sqlite3.connect('TicketPrint.db')
+db.init_db(conn)
+conn.close()
